abe17d1d300a31693d3b6c60fe703a5cadc60b9e
elxp101t3r <eliotx10101@outlook.com>
2024-02-11 20:10:16 +0200

    Completed!

--- main.py
+++ main.py
@@ -3,41 +3,47 @@ import pandas as pd
 from random import choice
 import smtplib
 
+class Mailer:
+        def __init__(self):
+                self.your_mail = 'your mail'
+                self.password = 'your pass'
+                self.templates = ['template1.txt', 'template2.txt', 'template3.txt']
+                self.data = pd.read_csv('birthdays.csv', index_col=False)
+                self.now = dt.datetime.now()
+                self.todays_birthdays = self.data[(self.data['month'] == self.now.month) & (self.data['day'] == self.now.day)]
+                
+                
+        def send_mail(self):
+                if not self.todays_birthdays.empty:
 
-your_mail = 'your mail'
-password = 'your pass'
-
-
-templates = ['template1.txt', 'template2.txt', 'template3.txt']
-data = pd.read_csv('birthdays.csv', index_col=False)
-
-now = dt.datetime.now()
-
-todays_birthdays = data[(data['year'] == now.year) & (data['month'] == now.month) & (data['day'] == now.day)]
-
-today_name = todays_birthdays['name'].values[0]
-address = todays_birthdays['email'].values[0]
-r_choice = choice(templates)
-
-
-with open(f'./templates/{r_choice}', 'r') as f:
-        txt_tmp = f.readlines()
-
-for i in range(len(txt_tmp)):
-    if '[Name]' in txt_tmp[i]:
-            txt_tmp[i] = today_name
-    elif '[Your Name]' in txt_tmp[i]:
-            txt_tmp[i] = 'Kostas'
-
-msg = ''.join(txt_tmp)
+                        today_name = self.todays_birthdays['name'].values[0]
+                        address = self.todays_birthdays['email'].values[0]
+                        r_choice = choice(self.templates)
 
 
+                        with open(f'./templates/{r_choice}', 'r') as f:
+                                txt_tmp = f.readlines()
 
-with smtplib.SMTP('smtp.gmail.com') as connection:
-        connection.starttls()
-        connection.login(user=your_mail, password=password)
-        connection.sendmail(
-                from_addr=your_mail, 
-                to_addrs=address, 
-                msg=f'Subject:Happy Birthday!\n\n{msg}'
-        )
+                        for i in range(len(txt_tmp)):
+                                if '[Name]' in txt_tmp[i]:
+                                        txt_tmp[i] = today_name
+                                elif '[Your Name]' in txt_tmp[i]:
+                                        txt_tmp[i] = 'Kostas'
+                        msg = ''.join(txt_tmp)
+
+
+
+                        with smtplib.SMTP('smtp.gmail.com') as connection:
+                                connection.starttls()
+                                connection.login(user=self.your_mail, password=self.password)
+                                connection.sendmail(
+                                        from_addr=self.your_mail, 
+                                        to_addrs=address, 
+                                        msg=f'Subject:Happy Birthday!\n\n{msg}'
+                                )
+                else:
+                        print('No birthdays today!')
+
+
+mail = Mailer()
+mail.send_mail()                     
