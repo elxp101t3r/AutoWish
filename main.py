272101686d41c689a7565bf3e840496500f9a42d
elxp101t3r <eliotx10101@outlook.com>
2024-02-11 19:47:04 +0200

    Add the mail sender

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import datetime as dt
 import pandas as pd
 from random import choice
+import smtplib
+
+
+your_mail = 'your mail'
+password = 'your pass'
+
 
 templates = ['template1.txt', 'template2.txt', 'template3.txt']
 data = pd.read_csv('birthdays.csv', index_col=False)
@@ -10,7 +16,7 @@ now = dt.datetime.now()
 todays_birthdays = data[(data['year'] == now.year) & (data['month'] == now.month) & (data['day'] == now.day)]
 
 today_name = todays_birthdays['name'].values[0]
-
+address = todays_birthdays['email'].values[0]
 r_choice = choice(templates)
 
 
@@ -23,4 +29,15 @@ for i in range(len(txt_tmp)):
     elif '[Your Name]' in txt_tmp[i]:
             txt_tmp[i] = 'Kostas'
 
-print(''.join(txt_tmp))
+msg = ''.join(txt_tmp)
+
+
+
+with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=your_mail, password=password)
+        connection.sendmail(
+                from_addr=your_mail, 
+                to_addrs=address, 
+                msg=f'Subject:Happy Birthday!\n\n{msg}'
+        )
